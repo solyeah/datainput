d54f4613b7528e221519a2538449193c2b55ca67
wh1tel10n <sm.whitelion@gmail.com>
2019-08-31 13:39:46 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -11,11 +11,6 @@ import CloseBidding from './components/CloseBidding'
 function App() {
   return (
     <div className="App">
-     
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-     
       <MemberInput />
       <VehicleInput />
       <VehicleListingInput />
